ee730580e3aa59c7d8d541b6c8890f045b7ddd3c
Brotha Darkness <gbiago961@gmail.com>
2017-08-30 14:44:29 -0400

    More scripts

--- Git_Cloner.sh
+++ Git_Cloner.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for links in $(cat targets);do
+for links in $(cat GitLinks.txt);do
 echo "----------------------------------------------------------"
+echo "You are downloading this repo:"
 echo $links
 echo "----------------------------------------------------------"
 git clone $links
