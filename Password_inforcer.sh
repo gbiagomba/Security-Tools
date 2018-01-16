db8b2a6b971ba3884d2971419f996c08ad4c6933
Gilles Biagomba <gbiago961@gmail.com>
2018-01-16 10:44:04 -0500

    Updated git_cloner to git_mngr

--- Password_inforcer.sh
+++ Password_inforcer.sh
@@ -6,5 +6,5 @@
 #https://jerrygamblin.com/2017/08/24/disallow-million-most-common-passwords/
 
 apt-get install libpam-cracklib -y
-wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_1000000.txt /usr/share/dict/ -O /usr/share/dict/million.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_100000.txt /usr/share/dict/ -O /usr/share/dict/million.txt
 create-cracklib-dict /usr/share/dict/million.txt
