8264905ad5368ed2acb36c8a95152ec9a0d040e3
Gilles Biagomba <gbiago961@gmail.com>
2018-03-12 16:19:09 -0400

    Housecleaning

--- WeakSSL/Sniper_SSL.sh
+++ WeakSSL/Sniper_SSL.sh
@@ -8,7 +8,7 @@ declare -a PORT=(22 25 443 567 593 808 1433 3389 4443 4848 7103 7201 8443 8888)
 #read workspace
 
 #Performing a Sniper scan
-for IP in $(cat targets); do
+for IP in $(cat $targets); do
     for PORTNUM in ${PORT[*]};do
 		sniper -w $workspace -t $IP -m webporthttps -p $PORTNUM
 	done
