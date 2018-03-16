c9a021695a03395b6a9a856bd28eea752ea16356
Gilles Biagomba <gbiago961@gmail.com>
2018-03-16 17:33:30 -0400

    Updated git mngr

--- Git_Tools/Git_Mngr.sh
+++ Git_Tools/Git_Mngr.sh
@@ -5,28 +5,98 @@
 # 	      Open the GitLinks.txt file, copy all git links into it.\n
 #	      Save and close the file, then run Git_Cloner.sh.\n
 
-ls > GITPATHTEMP.txt
-cat GITPATHTEMP.txt | grep -v GITPATHTEMP.txt > GITPATH.txt
+#Setting path to working directory
+ORGPATH=$(pwd)
 
-for pths in $(cat GITPATH.txt);do
-cd $pths
-echo "----------------------------------------------------------"
-echo "You are updating this Git repo:"
-pwd
-echo "----------------------------------------------------------"
-git pull
-cd ..
-done
+#Updating existing git repos
+function GitUpdate()
+{    
+    echo " " > GITPATHTEMP.txt
+    ls > GITPATHTEMP.txt
+    cat GITPATHTEMP.txt | grep -v GITPATHTEMP.txt > GITPATH.txt
 
-for links in $(cat GitLinks.txt);do
-echo "----------------------------------------------------------"
-echo "You are downloading this Git repo:"
-echo $links
-echo "----------------------------------------------------------"
-git clone $links
-done
+    for pths in $(cat GITPATH.txt);do
+        cd $pths
+        echo "----------------------------------------------------------"
+        echo "You are updating this Git repo:"
+        echo $pths
+        echo "----------------------------------------------------------"
+        git pull
+        cd ..
+    done
+}
 
-rm GITPATHTEMP.txt GITPATH.txt
-unset pths
-unset links
-set -u
+#Downloading new git repos
+function GitLinks()
+{
+    for links in $(cat GitLinks.txt);do
+        echo "----------------------------------------------------------"
+        echo "You are downloading this Git repo:"
+        echo $links
+        echo "----------------------------------------------------------"
+        git clone $links
+    done
+}
+
+#Pause on exit
+function pause()
+{
+   read -p "$*"
+}
+
+#De-initialize all variables & setting them to NULL
+function destructor()
+{
+    rm $ORGPATH/GITPATHTEMP.txt $ORGPATH/GITPATH.txt -rf
+    unset answer
+    unset links
+    unset ORGPATH
+    unset pths
+    set -u
+}
+
+#User selection
+function UserSelect()
+{
+    echo
+    echo "What do you want to do?"
+    echo "Enter 1 to update existing repos"
+    echo "Enter 2 to download new repos"
+    echo "Enter 3 to do all of the above"
+    echo "Enter 4 to exit"
+    read answer
+    echo
+
+    if [ "$answer" != "1" ] && [ "$answer" != "2" ] && [ "$answer" != "3" ] && [ "$answer" != "4" ];then
+        UserSelect
+    fi
+
+    #Switch case
+    case $answer in
+        1)
+            GitUpdate
+            destructor
+            UserSelect
+            ;;
+        2)
+            GitLinks
+            destructor
+            UserSelect
+            ;;
+        3)
+            GitUpdate
+            GitLinks
+            destructor
+            UserSelect
+            ;;
+        4)
+            echo "Have a good day"
+            pause 'Press [Enter] key to exit...'
+            destructor
+            clear
+            exit
+            ;;
+    esac
+}
+
+UserSelect
