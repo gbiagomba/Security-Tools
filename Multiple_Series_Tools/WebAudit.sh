7d2f7cce0832718a322c8316d57137e6ef227102
Gilles Biagomba <gbiago961@gmail.com>
2018-05-24 01:12:09 -0400

    Updating repo

--- Multiple_Series_Tools/WebAudit.sh
+++ Multiple_Series_Tools/WebAudit.sh
@@ -9,6 +9,6 @@ PTH=$(pwd)
 sniper -w $workspace -f $PTH/$targets -m nuke
 
 for url in $(cat $targets);do
-	dirb $url /root/.ZAP/fuzzers/dirbuster/directory-list-1.0.txt -o dirb_output.txt -w
-	nikto -C all -h $url -o Nikto_Output.txt
+	nikto -C all -h https://$url -o Nikto_Output.txt
+	dirb $url /root/.ZAP/fuzzers/dirbuster/directory-list-1.0.txt -o dirb_output.txt -w	
 done
--- Nmap_Scripts/NmapScript_Full.sh
+++ Nmap_Scripts/NmapScript_Full.sh
@@ -2,28 +2,41 @@
 #pth=/root/Clients/CLIENTFOLDER
 pth=$(pwd)
 
+# Setting up workspace
+mkdir -p ICMP PING SERV REPORTS
+
+# Project name
+echo "What is the name of the project?"
+read workspace
+echo
+
 # HOST DISCOVERY
 
 # Nmap - Pingsweep using ICMP echo
 nmap -sP -PE -iL $pth/targets -oA $pth/icmpecho
 cat $pth/icmpecho.gnmap | grep Up | cut -d ' ' -f 2 > $pth/live
+xsltproc icmpecho.xml -o icmpecho.html
 
 # Nmap - Pingsweep using ICMP timestamp
 nmap -sP -PP -iL $pth/targets -oA $pth/icmptimestamp
 cat $pth/icmptimestamp.gnmap | grep Up | cut -d ' ' -f 2 >> $pth/live
+xsltproc icmptimestamp.xml -o icmptimestamp.html
 
 # Nmap - Pingsweep using ICMP netmask
 nmap -sP -PM -iL $pth/targets -oA $pth/icmpnetmask
 cat $pth/icmpnetmask.gnmap | grep Up | cut -d ' ' -f 2 >> $pth/live
+xsltproc icmpnetmask.xml -o icmpnetmask.html
 
 # Systems that respond to ping (finding)
 cat $pth/live | sort | uniq > $pth/pingresponse
 
 # Nmap - Pingsweep using TCP SYN and UDP
-nmap -sP -PS21,22,23,25,53,80,88,110,111,135,139,443,445,8080 -iL $pth/targets -oA $pth/pingsweepTCP
-nmap -sP -PU53,111,135,137,161,500 -iL $pth/targets -oA $pth/pingsweepUDP
+nmap -sP -PS 21,22,23,25,53,80,88,110,111,135,139,443,445,8080 -iL $pth/targets -oA $pth/pingsweepTCP
+nmap -sP -PU 53,111,135,137,161,500 -iL $pth/targets -oA $pth/pingsweepUDP
 cat $pth/pingsweepTCP.gnmap | grep Up | cut -d ' ' -f 2 >> $pth/live
 cat $pth/pingsweepUDP.gnmap | grep Up | cut -d ' ' -f 2 >> $pth/live
+xsltproc pingsweepTCP.xml -o pingsweepTCP.html
+xsltproc pingsweepUDP.xml -o pingsweepUDP.html
 
 # Create unique live hosts file
 cat $pth/live | sort | uniq > $pth/livehosts
@@ -31,23 +44,48 @@ cat $pth/live | sort | uniq > $pth/livehosts
 # PORT SCANNING
 
 # Nmap - Full TCP SYN scan on live targets
-nmap -sS -PN -O -sV -T4 -p0-65535 -iL $pth/livehosts -oA $pth/TCPdetails
+nmap -sS -PN -O -sV -T4 -R -p0-65535 -script=vulners -iL $pth/livehosts -oA $pth/TCPdetails
 cat $pth/TCPdetails.gnmap | grep ' 25/open' | cut -d ' ' -f 2 > $pth/SMTP
 cat $pth/TCPdetails.gnmap | grep ' 53/open' | cut -d ' ' -f 2 > $pth/DNS
 cat $pth/TCPdetails.gnmap | grep ' 23/open' | cut -d ' ' -f 2 > $pth/telnet
 cat $pth/TCPdetails.gnmap | grep ' 445/open' | cut -d ' ' -f 2 > $pth/SMB
 cat $pth/TCPdetails.gnmap | grep ' 139/open' | cut -d ' ' -f 2 > $pth/netbios
+cat $pth/TCPdetails.gnmap | grep http | grep open | cut -d ' ' -f 2 > $pth/http
+cat $pth/TCPdetails.gnmap | grep ssl | grep open | cut -d ' ' -f 2 > $pth/ssh
+cat $pth/TCPdetails.gnmap | grep ssl | grep open | cut -d ' ' -f 2 > $pth/ssl
+xsltproc TCPdetails.xml -o TCPdetails.html
 
 # Nmap - Default UDP scan on live targets
 nmap -sU -PN -T4 -iL $pth/livehosts -oA $pth/UDPdetails
 cat $pth/UDPdetails.gnmap | grep ' 161/open\?\!|' | cut -d ' ' -f 2 > $pth/SNMP
 cat $pth/UDPdetails.gnmap | grep ' 500/open\?\!|' | cut -d ' ' -f 2 > $pth/isakmp
+xsltproc UDPdetails.xml -o UDPdetails.html
 
 # Nmap - Scan for possible vulnerabilities using vulners
-nmap -sV -sS -sU --script=vulners -R -iL target2 -A -p 0-65535-iL $pth/livehosts -oA $pth/VulScan
+# nmap -sV -sS -sU --script=vulners -R -iL target2 -A -p 0-65535-iL $pth/livehosts -oA $pth/VulScan
 
 # Empty file cleanup
 find $pth -size 0c -type f -exec rm -rf {} \;
 
-# echo "$pth nmap host discovery done" | sendmail "<Enter Email Address>"
+# Organizing directory
+mv *.html REPORTS/
+mv icmp*.* ICMP/
+mv ping*.* PING/
+mv SMTP SERV/
+mv DNS SERV/
+mv telnet SERV/
+mv SMB SERV/
+mv netbios SERV/
+mv http SERV/
+mv ssh SERV/
+mv ssl SERV/
+mv SNMP SERV/
+mv isakmp SERV/
 
+# Gift wrap the findings ;)
+zip -r $workspace.zip $pth
+zipcloak $workspace.zip -O $workspace-secured.zip -q
+
+# Send an email to the team!
+#thunderbird -compose "to='email@example.com','attachment=blah.html'"
+thunderbird -compose "to='jfoy@nbme.org,gbiagomba@nbme.org',cc='sgebeline@nbme.org',subject='$workspace Network Discovery Scan',body='The discovery scan finished and attached are the findings!',attachment='$pth/$workspace.zip"
