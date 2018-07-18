40e883d969a98193a6596d4d3b6a6029affa2850
Gilles B <gbiago961@gmail.com>
2018-07-18 16:11:19 -0400

    some housecleaning

--- BATCH_Tools/Bash/JBOSSter.sh
+++ BATCH_Tools/Bash/JBOSSter.sh
@@ -10,7 +10,7 @@
 declare -a PORT=(443 465 563 585 636 695 832 898 981 989 990 992 993 994 995 1090 1098 1099 1311 1360 1433 1434 1521 1527 1583 2083 2087 2096 2376 2484 2638 3269 3306 3351 3389 3424 3873 4443 4445 4446 4843 5223 5432 5500 5800 5900 6080 6432 6619 6679 6697 7000 7002 7080 7091 7092 7101 7103 7105 7107 7109 7306 7307 8009 8080 8081 8083 8243 8333 8443 8531 8888 9001 9091 9443 10911 11214 11215 12043 12443 12975 13722 18091 18092 18366 19812 20911 23642 27724 32976 33300 33840 36210 37549 38131 38760 41443 41581 41971 43778 46160 46393 55130 55443 62657 64779)
 App="jexboss"
 diskMax=90
-diskSize=$(df | grep /dev/sda1 | cut -d " " -f 11 | cut -d "%" -f 1)
+diskSize=$(df | grep /dev/sda1 | cut -d " " -f 13 | cut -d "%" -f 1)
 pth=$(pwd)
 RunTIME=$(date +%H:%M)
 TodaysDAY=$(date +%m-%d)
@@ -19,7 +19,9 @@ TodaysYEAR=$(date +%Y)
 workpth="$pth/$TodaysYEAR/$TodaysMONTH/$TodaysDAY/$RunTIME/"
 
 #Checking system resources (HDD space)
-if ["$diskSize" -ge "$diskMax"];then
+if [ "$diskSize" -ge "$diskMax" ];then
+	clear
+	echo 
 	echo "You are using $diskSize% and I am concerned you might run out of space"
 	echo "Remove some files and try again, you will thank me later, trust me :)"
 	exit
@@ -36,16 +38,25 @@ cd $pth
 mkdir -p $workpth/Masscan $workpth/wildPwn/ $workpth/SerializeKiller/ $workpth/Nmap
 mkdir -p $workpth/JexBoss/Logs $workpth/Sniper $workpth/Nikto $workpth/Reports
 
+#Requesting target file 
+clear
+echo
+echo "What is the name of the targets file? The file with all the IP addresses"
+read targets
+
 #Launching scans
 
 #Use masscan to perform a quick port sweep
-masscan -iL $pth/targets -p 0-65535 --open-only --banners -oL $workpth/Masscan/masscan_output
+cat $targets | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > temptargets
+cat $targets | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\/[0-9]\{1,\}'  >> temptargets
+cat temptargets | sort | uniq > targets2
+masscan -iL $pth/targets2 -p 0-65535 --open-only --banners -oL $workpth/Masscan/masscan_output
 #Above the above stores the openports list to the variable to be later sent to nmap
 OpenPORT=($(cat $workpth/Masscan/masscan_output | cut -d " " -f 3 | grep -v masscan | sort | uniq))
 
 #Use nmap to perform a targeted scan then sorting the various hosts based on their technologies
 cd $pth
-nmap -A -Pn -R -sS -sU -sV --script=ssl-enum-ciphers,vulners,wildfly-detect -iL $pth/targets -p $(echo ${OpenPORT[*]} | sed 's/ /,/g') -oA $workpth/Nmap/NBME_JBOSS
+nmap -A -Pn -R -sS -sU -sV --script=ssl-enum-ciphers,vulners,wildfly-detect -iL $pth/$targets -p $(echo ${OpenPORT[*]} | sed 's/ /,/g') -oA $workpth/Nmap/NBME_JBOSS
 cat $workpth/Nmap/NBME_JBOSS.gnmap | grep "Up" | cut -d " " -f 2 | sort | uniq > $workpth/Nmap/livehosts
 cat $workpth/Nmap/NBME_JBOSS.gnmap | grep -i "jboss" | cut -d " " -f 2 | sort | uniq > $workpth/Nmap/jboss
 cat $workpth/Nmap/NBME_JBOSS.gnmap | grep -i "java" | cut -d " " -f 2 | sort | uniq > $workpth/Nmap/java
@@ -67,7 +78,7 @@ for IP in $(cat $workpth/Nmap/livehosts); do
 		STAT3=$(cat $workpth/Nmap/NBME_JBOSS.gnmap | grep $IP | grep "$PORTNUM/filtered" -m 1 -o | grep "filtered" -o)
 		if [ "$STAT1" == "Up" ] && [ "$STAT2" == "open" ] || [ "$STAT3" == "filtered" ];then
 			if [ "$PORTNUM" == "443" ]
-				python jexboss.py -u https://$IP:443 -results "$workpth/JexBoss/$IP-$PORTNUM-HTTPS-JexBOSS" -out "$workpth/JexBoss/Logs/$IP-$PORTNUM-report_file_scan2.log" | tee -a $workpth/JexBoss/Logs/$IP-$PORTNUM
+				python jexboss.py -u https://$IP -results "$workpth/JexBoss/$IP-$PORTNUM-HTTPS-JexBOSS" -out "$workpth/JexBoss/Logs/$IP-$PORTNUM-report_file_scan2.log" | tee -a $workpth/JexBoss/Logs/$IP-$PORTNUM
 			fi
 			python jexboss.py -u http://$IP:$PORTNUM -results "$workpth/JexBoss/$IP-$PORTNUM-HTTP-JexBOSS" -out "$workpth/JexBoss/Logs/$IP-$PORTNUM-report_file_scan.log" | tee $workpth/JexBoss/$IP-$PORTNUM
 			python jexboss.py -u https://$IP:$PORTNUM -results "$workpth/JexBoss/$IP-$PORTNUM-HTTPS-JexBOSS" -out "$workpth/JexBoss/Logs/$IP-$PORTNUM-report_file_scan2.log" | tee -a $workpth/JexBoss/Logs/$IP-$PORTNUM
