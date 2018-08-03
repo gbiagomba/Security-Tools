1f0d8a6a6ef315744e1e524d8dac7573f0a200ee
Gilles Biagomba <gbiago961@gmail.com>
2018-08-03 11:28:23 -0400

    Housecleaning

---
+++ BATCH_Tools/Bash/GraveDigger.sh
@@ -0,0 +1,38 @@
+# Author: Gilles Biagomba
+# Program: GraveDigger.sh
+# Description: This script was designed to mass copy files and zip them.\n
+
+# Setting working directory
+pth=$(pwd)
+
+# Prompting user for questions
+echo "What file(s) would you like to find?"
+read FTYPE
+echo "What is keyword/filter you would like to use?"
+read FILTER
+echo "What is the name of the zip file you would like to use?"
+read ZFILE
+
+# Updating the file databse
+updatedb
+
+# Locating the fie(s) in question
+if [ -z $FILTER ]; then
+	FILES=($(locate *.$FTYPE))
+else
+	FILES=($(locate *.$FTYPE | grep $FILTER))
+fi
+
+# Zipping up said files
+for FILE in ${FILES[*]}; do
+	zip -ru -9 $pth/$ZFILE $FILE
+done
+
+# CLeaning up
+unset pth
+unset FTYPE
+unset FILTER
+unset ZFILE
+unset FILES
+unset FILE
+set -u
