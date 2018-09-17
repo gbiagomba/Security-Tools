b124b56d64228c3a951e7ed284afc4798df9c6f4
Gilles B <gbiago961@gmail.com>
2018-09-17 15:30:34 -0400

    Added nmap scan and updated README

--- Sweet32_Birthday/Birthday_test.sh
+++ Sweet32_Birthday/Birthday_test.sh
@@ -2,8 +2,17 @@
 #Author: Gilles Biagomba
 #Program: Birthday_test.sh
 #Description: This script was design check for CVE-2016-2183, CVE-2016-6329.\n
-# 	      Or aka Sweet32: Birthday attacks on 64-bit block ciphers in TLS and OpenVPN.\n
+# 	      Or aka Sweet32: Birthday attacks on 64-bit block ciphers in TLS/SSL and OpenVPN.\n
 #	      https://sweet32.info/ \n
+
+
+# Nmap Scan
+echo "--------------------------------------------------"
+echo "Performing the SSL scan using Nmap"
+echo "--------------------------------------------------"
+nmap -A -F -Pn -R -sS -sU -sV --script=ssl-enum-ciphers,vulners -iL targets -oA Swett_Thirty-two
+xsltproc Swett_Thirty-two.xml -o Reports/Nmap_TLS_Output.html
+cat Swett_Thirty-two.gnmap | grep Up | cut -d ' ' -f 2 > live
 
 for c in $(cat targets); do
  for i in $(cat WeakCiphers.txt); do 
