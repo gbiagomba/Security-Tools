e4612aed23b28f002707cf1dea9db5f8ffb6b192
Gilles Biagomba <gbiago961@gmail.com>
2019-01-11 17:14:12 -0500

    Updated ntwrk_srvc_reboot.sh to include a third connection check

--- Sys_Mng_Scripts/ntwrk_srvc_reboot.sh
+++ Sys_Mng_Scripts/ntwrk_srvc_reboot.sh
@@ -7,10 +7,6 @@
 mkdir -p /tmp/Net-Tests/
 cd /tmp/Net-Tests/
 
-# Declaring variables
-STATUS1=$(ifconfig | grep -o -m 1 -h eth)
-STATUS2=$(ifconfig | grep -o -m 1 -h wlan)
-
 # Capturing request from user
 srvrname=$1
 if [ -z $1 ] || [ -z $srvrname ]; then
@@ -20,21 +16,28 @@ if [ -z $1 ] || [ -z $srvrname ]; then
 	srvrname="www.google.com"
 fi
 
+# Declaring variables
+STATUS1=$(ifconfig | grep -o -m 1 -h eth)
+STATUS2=$(ifconfig | grep -o -m 1 -h wlan)
+STATUS3=$(host $srvrname | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+
 # Checking internet status
-if [ "$STATUS1" != "eth" ] || [ "$STATUS2" != "wlan" ]; then
+if [ "$STATUS1" != "eth" ] || [ "$STATUS2" != "wlan" ] || [ -z "$STATUS3" ]; then
 	service networking restart
 	service network-manager restart
 fi
 
 # Viewing IP address
-ifconfig | aha > ip_address.html
+ifconfig | tee ip_address.txt
+cat  ip_address.txt | aha > ip_address.html
 
 # Quick ping sweep
 nmap -PE -PM -PP -R --reason --resolve-all -sP -oA NetworkTest $srvrname
 xsltproc NetworkTest.xml -o NetworkTest.html
 
 # Viewing results
-gnome-terminal --tab -- firefox --new-tab ip_address.html NetworkTest.html &
+# gnome-terminal --tab -- 
+firefox --new-tab ip_address.html NetworkTest.html &
 
 # ALL
 echo "Have a nice day :)"
